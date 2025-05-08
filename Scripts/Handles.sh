9e26cad3c72c2e271ee445c0cb853f60ecc1dc3a
pawaluodiyang <44284231+pawaluodiyang@users.noreply.github.com>
2025-05-08 15:05:04 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -2,25 +2,25 @@
 
 PKG_PATH="$GITHUB_WORKSPACE/wrt/package/"
 
-#预置HomeProxy数据
-if [ -d *"homeproxy"* ]; then
-	HP_RULE="surge"
-	HP_PATH="homeproxy/root/etc/homeproxy"
+# #预置HomeProxy数据
+# if [ -d *"homeproxy"* ]; then
+# 	HP_RULE="surge"
+# 	HP_PATH="homeproxy/root/etc/homeproxy"
 
-	rm -rf ./$HP_PATH/resources/*
+# 	rm -rf ./$HP_PATH/resources/*
 
-	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./$HP_RULE/
-	cd ./$HP_RULE/ && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
+# 	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./$HP_RULE/
+# 	cd ./$HP_RULE/ && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
 
-	echo $RES_VER | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
-	awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
-	sed 's/^\.//g' direct.txt > china_list.txt ; sed 's/^\.//g' gfw.txt > gfw_list.txt
-	mv -f ./{china_*,gfw_list}.{ver,txt} ../$HP_PATH/resources/
+# 	echo $RES_VER | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
+# 	awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
+# 	sed 's/^\.//g' direct.txt > china_list.txt ; sed 's/^\.//g' gfw.txt > gfw_list.txt
+# 	mv -f ./{china_*,gfw_list}.{ver,txt} ../$HP_PATH/resources/
 
-	cd .. && rm -rf ./$HP_RULE/
+# 	cd .. && rm -rf ./$HP_RULE/
 
-	cd $PKG_PATH && echo "homeproxy date has been updated!"
-fi
+# 	cd $PKG_PATH && echo "homeproxy date has been updated!"
+# fi
 
 #修改argon主题字体和颜色
 if [ -d *"luci-theme-argon"* ]; then
@@ -32,21 +32,21 @@ if [ -d *"luci-theme-argon"* ]; then
 	cd $PKG_PATH && echo "theme-argon has been fixed!"
 fi
 
-#修改qca-nss-drv启动顺序
-NSS_DRV="../feeds/nss_packages/qca-nss-drv/files/qca-nss-drv.init"
-if [ -f "$NSS_DRV" ]; then
-	sed -i 's/START=.*/START=85/g' $NSS_DRV
+# #修改qca-nss-drv启动顺序
+# NSS_DRV="../feeds/nss_packages/qca-nss-drv/files/qca-nss-drv.init"
+# if [ -f "$NSS_DRV" ]; then
+# 	sed -i 's/START=.*/START=85/g' $NSS_DRV
 
-	cd $PKG_PATH && echo "qca-nss-drv has been fixed!"
-fi
+# 	cd $PKG_PATH && echo "qca-nss-drv has been fixed!"
+# fi
 
-#修改qca-nss-pbuf启动顺序
-NSS_PBUF="./kernel/mac80211/files/qca-nss-pbuf.init"
-if [ -f "$NSS_PBUF" ]; then
-	sed -i 's/START=.*/START=86/g' $NSS_PBUF
+# #修改qca-nss-pbuf启动顺序
+# NSS_PBUF="./kernel/mac80211/files/qca-nss-pbuf.init"
+# if [ -f "$NSS_PBUF" ]; then
+# 	sed -i 's/START=.*/START=86/g' $NSS_PBUF
 
-	cd $PKG_PATH && echo "qca-nss-pbuf has been fixed!"
-fi
+# 	cd $PKG_PATH && echo "qca-nss-pbuf has been fixed!"
+# fi
 
 #移除Shadowsocks组件
 PW_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-passwall/Makefile")
@@ -75,10 +75,10 @@ if [ -f "$TS_FILE" ]; then
 	cd $PKG_PATH && echo "tailscale has been fixed!"
 fi
 
-#修复Coremark编译失败
-CM_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/coremark/Makefile")
-if [ -f "$CM_FILE" ]; then
-	sed -i 's/mkdir/mkdir -p/g' $CM_FILE
+# #修复Coremark编译失败
+# CM_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/coremark/Makefile")
+# if [ -f "$CM_FILE" ]; then
+# 	sed -i 's/mkdir/mkdir -p/g' $CM_FILE
 
-	cd $PKG_PATH && echo "coremark has been fixed!"
-fi
+# 	cd $PKG_PATH && echo "coremark has been fixed!"
+# fi
